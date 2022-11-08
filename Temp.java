48da740ea8ea8fb93988004fe1cb66cb1101758c
21377 <t@vtsd.com>
2022-11-08 08:35:51 -0500

    ty 5

--- Temp.java
+++ Temp.java
@@ -1,3 +1,19 @@
-public class Temp {
+import java.applet.Applet;
+import java.awt.*;
+import java.util.Random;
+public class Temp extends Applet  {
+    public void paint(Graphics g){
+        for(int k=1; k<= 100; k++){
+        Random rand = new Random();
 
+        int x1 = rand.nextInt(1000);
+        int y1 = rand.nextInt(650);
+        int width = rand.nextInt(400);
+        int height = rand.nextInt(400);
+
+
+
+        g.drawRect(x1, y1, width, height);}
+
+    }
 }
